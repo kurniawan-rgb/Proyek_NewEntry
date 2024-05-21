1d8a87c60a04d16271b390d773f211769648c624
Richal Akbar <richalakbarical@gmail.com>
2024-05-21 21:13:00 +0800

    update frontend

--- src/components/navbarComponent.vue
+++ src/components/navbarComponent.vue
@@ -1,79 +1,96 @@
 <template>
-  <div style="margin-bottom: 65px;">
-  <nav class=" fixed-button-admin navbar navbar-expand-lg custom-bg-color-navbar">
-    <div class="container-fluid custom-bg-color">
-      <router-link class="navbar-brand d-flex align-items-center" to="/">
-        <img
-          src="https://www.unhas.ac.id/wp-content/uploads/2022/09/Logo-Resmi-Unhas-1.png"
-          alt="Logo"
-          style="margin-right: 10px"
-          width="35"
-          height="40"
-          class="d-inline-block align-text-top"
-        />
-        <span>New Entry</span>
-      </router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-        @click="toggleMenu"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link class="nav-link" :class="{ active: isActive('/') }" to="/">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" :class="{ active: isActive('/dashboard') }" to="/dashboard">Dashboard</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" :class="{ active: isActive('/outputmenu') }" to="/outputmenu">View</router-link>
-          </li>
-        </ul>
-        <div class="d-flex align-items-center" v-if="isDesktop">
+  <div style="margin-bottom: 45px">
+    <nav
+      class="fixed-button-admin navbar navbar-expand-lg custom-bg-color-navbar"
+    >
+      <div class="container-fluid custom-bg-color">
+        <router-link class="navbar-brand d-flex align-items-center" to="/">
           <img
-            src="D:/Proyek_NewEntry-master/src/assets/User Male.png"
-            alt="User Avatar"
-            width="35"
-            height="35"
+            src="https://www.unhas.ac.id/wp-content/uploads/2022/09/Logo-Resmi-Unhas-1.png"
+            alt="Logo"
             style="margin-right: 10px"
+            width="35"
+            height="40"
+            class="d-inline-block align-text-top"
           />
-          <p class="mb-lg-0" style="margin-right: 15px">Akun Saya</p>
-        </div>
-        <div v-else>
-          <ul class="navbar-nav mb-2 mb-lg-0">
+          <span>New Entry</span>
+        </router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          @click="toggleMenu"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <router-link
-                to="/login"
-                class="border-btn btn-navbar btn-primary button-wrapper-outputmenu"
+                class="nav-link"
+                :class="{ active: isActive('/') }"
+                to="/"
+                >Home</router-link
               >
-                Log Out
-              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                :class="{ active: isActive('/adminuser') }"
+                to="/adminuser"
+                >Dashboard</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                :class="{ active: isActive('/outputmenu') }"
+                to="/outputmenu"
+                >View</router-link
+              >
             </li>
           </ul>
-        </div>
-        <div class="border-btn" v-if="isDesktop">
-          <router-link
-            to="/login"
-            class="button-wrapper-outputmenu"
-            style="display: flex; align-items: center"
-          >
-            <button type="button" class="border-btn btn-navbar btn-primary">
-              Log Out
-            </button>
-          </router-link>
+          <div class="d-flex align-items-center" v-if="isDesktop">
+            <img
+              src="D:/Proyek_NewEntry-master/src/assets/User Male.png"
+              alt="User Avatar"
+              width="35"
+              height="35"
+              style="margin-right: 10px"
+            />
+            <p class="mb-lg-0" style="margin-right: 15px">Akun Saya</p>
+          </div>
+          <div v-else>
+            <ul class="navbar-nav mb-2 mb-lg-0">
+              <li class="nav-item">
+                <router-link
+                  to="/login"
+                  class="border-btn btn-navbar btn-primary button-wrapper-outputmenu"
+                >
+                  Log Out
+                </router-link>
+              </li>
+            </ul>
+          </div>
+          <div class="border-btn" v-if="isDesktop">
+            <router-link
+              to="/login"
+              class="button-wrapper-outputmenu"
+              style="display: flex; align-items: center"
+            >
+              <button type="button" class="border-btn btn-navbar btn-primary">
+                Log Out
+              </button>
+            </router-link>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
-</div>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -102,7 +119,7 @@ export default {
     },
     isActive(route) {
       return this.$route.path === route;
-    }
+    },
   },
   beforeDestroy() {
     window.removeEventListener("resize", this.checkScreenSize);
@@ -120,6 +137,10 @@ export default {
 }
 
 .nav-link.active {
-  color: rgb(0, 0, 0); /* atau warna apa pun yang Anda inginkan untuk menandai sebagai aktif */
+  color: rgb(
+    0,
+    0,
+    0
+  ); /* atau warna apa pun yang Anda inginkan untuk menandai sebagai aktif */
 }
 </style>
--- src/views/adminUser.vue
+++ src/views/adminUser.vue
@@ -6,46 +6,111 @@
       <div class="content-admin-user">
         <table class="table">
           <thead>
-            <tr>
+            <tr class="th-middle">
               <th class="text-center" scope="col">No</th>
               <th scope="col">User</th>
               <th scope="col">Fakultas</th>
               <th scope="col">Prodi</th>
+              <th scope="col">Username</th>
+              <th scope="col">Password</th>
               <th class="text-center" scope="col">WCU (QS/THE)</th>
               <th class="text-center" scope="col">NU Greenmetric</th>
               <th class="text-center" scope="col">SNI Award</th>
+              <th class="text-center action-column" scope="col">Actions</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th class="text-center" scope="row">1</th>
-              <td>Richal Akbar</td>
-              <td>Teknik</td>
-              <td>Teknik Informatika</td>
-              <th><iconPlus /></th>
-              <th><iconPlus /></th>
-              <th><iconPlus /></th>
-            </tr>
-            <tr>
-              <th class="text-center" scope="row">2</th>
-              <td>Andi Muhammad Sabar</td>
-              <td>Teknik</td>
-              <td>Teknik Informatika</td>
-              <th><iconPlus /></th>
-              <th><iconPlus /></th>
-              <th><iconPlus /></th>
-            </tr>
-            <tr>
-              <th class="text-center" scope="row">3</th>
-              <td>Reza Kurniawan</td>
-              <td>Teknik</td>
-              <td>Teknik Informatika</td>
-              <th><iconPlus /></th>
-              <th><iconPlus /></th>
-              <th><iconPlus /></th>
+            <tr v-for="(user, index) in users" :key="index">
+              <th class="text-center th-middle" scope="row">{{ index + 1 }}</th>
+              <td
+                v-if="!user.isNew && !user.isEditing"
+                class="text-left td-middle"
+              >
+                {{ user.name }}
+              </td>
+              <td v-else><input v-model="user.name" class="table-input" /></td>
+              <td
+                v-if="!user.isNew && !user.isEditing"
+                class="text-left td-middle"
+              >
+                {{ user.fakultas }}
+              </td>
+              <td v-else>
+                <input v-model="user.fakultas" class="table-input" />
+              </td>
+              <td
+                v-if="!user.isNew && !user.isEditing"
+                class="text-left td-middle"
+              >
+                {{ user.prodi }}
+              </td>
+              <td v-else><input v-model="user.prodi" class="table-input" /></td>
+              <td
+                v-if="!user.isNew && !user.isEditing"
+                class="text-left td-middle"
+              >
+                {{ user.username }}
+              </td>
+              <td v-else>
+                <input v-model="user.username" class="table-input" />
+              </td>
+              <td
+                v-if="!user.isNew && !user.isEditing"
+                class="text-left td-middle"
+              >
+                {{ user.password }}
+              </td>
+              <td v-else>
+                <input v-model="user.password" class="table-input" />
+              </td>
+              <td class="text-center td-middle">
+                <iconPlus v-if="!user.isNew && !user.isEditing" />
+              </td>
+              <td class="text-center td-middle">
+                <iconPlus v-if="!user.isNew && !user.isEditing" />
+              </td>
+              <td class="text-center td-middle">
+                <iconPlus v-if="!user.isNew && !user.isEditing" />
+              </td>
+              <td v-if="user.isNew" class="text-center action-column">
+                <button @click="addUser(index)" class="action-btn add-btn">
+                  Tambah
+                </button>
+              </td>
+              <td
+                v-else
+                class="text-center action-column edit-remove-container"
+              >
+                <button
+                  @click="editUser(index)"
+                  v-if="!user.isEditing"
+                  class="action-btn edit-btn"
+                >
+                  Edit
+                </button>
+                <button
+                  @click="saveUser(index)"
+                  v-else
+                  class="action-btn save-btn"
+                >
+                  Simpan
+                </button>
+                <button
+                  @click="removeUser(index)"
+                  style="margin-left: 5px"
+                  class="action-btn remove-btn"
+                >
+                  Hapus
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
+        <div>
+          <button @click="addNewRow" class="action-btn add-new-btn">
+            Tambah User
+          </button>
+        </div>
       </div>
     </main>
   </div>
@@ -62,5 +127,156 @@ export default {
     navbarAdmin,
     iconPlus,
   },
+  data() {
+    return {
+      users: [
+        {
+          name: "Richal Akbar",
+          fakultas: "Teknik",
+          prodi: "Teknik Informatika",
+          username: "d121211084",
+          password: "d1212110841967",
+        },
+        {
+          name: "Andi Muhammad Sabar",
+          fakultas: "Teknik",
+          prodi: "Teknik Informatika",
+          username: "d121211084",
+          password: "d1212110841967",
+        },
+        {
+          name: "Reza Kurniawan",
+          fakultas: "Teknik",
+          prodi: "Teknik Informatika",
+          username: "d121211084",
+          password: "d1212110841967",
+        },
+      ],
+    };
+  },
+  methods: {
+    addNewRow() {
+      this.users.push({
+        name: "",
+        fakultas: "",
+        prodi: "",
+        username: "",
+        password: "",
+        isNew: true,
+        isEditing: false,
+      });
+    },
+    addUser(index) {
+      this.users[index].isNew = false;
+    },
+    editUser(index) {
+      this.users[index].isEditing = true;
+    },
+    saveUser(index) {
+      this.users[index].isEditing = false;
+    },
+    removeUser(index) {
+      this.users.splice(index, 1);
+    },
+  },
 };
 </script>
+
+<style>
+.container-admin-user {
+  padding: 20px;
+}
+
+.table {
+  width: 100%;
+  margin-bottom: 20px;
+  border-collapse: collapse;
+}
+
+.table th,
+.table td {
+  border: 1px solid #dee2e6;
+  padding: 8px;
+  vertical-align: middle;
+}
+
+.table-input {
+  width: 100%;
+  box-sizing: border-box;
+  padding: 6px 12px;
+  border: 1px solid #ced4da;
+  border-radius: 4px;
+}
+
+.action-btn {
+  line-height: 1;
+  background-color: #2aaa94;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  width: auto;
+}
+
+.action-btn:hover {
+  background-color: #249e88;
+}
+
+.action-btn:active {
+  background-color: #208a76;
+}
+
+.action-btn:focus {
+  outline: none;
+}
+
+.remove-btn {
+  background-color: #e74c3c;
+  line-height: 1;
+}
+
+.remove-btn:hover {
+  background-color: #c0392b;
+}
+
+.remove-btn:active {
+  background-color: #922b21;
+}
+
+.text-left {
+  text-align: left;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.button-container {
+  text-align: center;
+}
+
+.edit-btn {
+  background-color: blue;
+  padding: 8px 16px;
+  line-height: 1;
+}
+
+.edit-btn:hover {
+  background-color: darkblue;
+}
+
+.edit-btn:active {
+  background-color: navy;
+}
+
+.td-middle,
+.th-middle {
+  vertical-align: middle;
+}
+
+.action-column {
+  width: 15ch;
+}
+</style>
